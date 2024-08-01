b47d72252fbab2af00870c26e6c8f7c3b43252cd
nesgtz12 <gtznes12@gmail.com>
2024-07-31 20:17:43 -0600

    Update configuration.py
    Aquí esta la dirección de "carpeta" de la creación de usuario y de los kits, también la URL de la app

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-ac4318d1-fb66-4551-ab88-41232555a23c.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users/"
+KITS_PATH = "/api/v1/kits/"
