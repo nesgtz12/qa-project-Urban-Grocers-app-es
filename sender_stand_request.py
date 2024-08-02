499f3b6c495f4ce6e3eec88cc4ba95e96d7ff8f3
nesgtz12 <gtznes12@gmail.com>
2024-08-01 20:23:49 -0600

    Update sender_stand_request.py
    Obtención de la información para la creación del usuario (auto token) y creación del kit

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -13,8 +13,15 @@ def create_count(body):
     )
 
 
-
 def create_kit(kit_body, auth_token):
+    # Verificar que auth_token no esté vacío
+    if not auth_token:
+        raise ValueError("El auth_token no puede estar vacío")
+
+    # Verificar que auth_token no exceda los 511 caracteres
+    if len(auth_token) > 512:
+        raise ValueError("El auth_token no puede tener más de 511 caracteres")
+
     headers = data.headers.copy()
     headers['Authorization'] = f'Bearer {auth_token}'
     return requests.post(
