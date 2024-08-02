6940f92e2998b550500f409cb0173af03b2590fb
nesgtz12 <gtznes12@gmail.com>
2024-08-01 20:22:20 -0600

    Update configuration.py
    URL del la aplicación, así como ubicación de los archivos (documentación-requisitos)

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-ac4318d1-fb66-4551-ab88-41232555a23c.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-f6262b17-3978-48cc-b5bb-2776dd389702.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"
